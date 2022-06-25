d7748d5573728c5d0f0e9e2594fc0c1b6da5449f
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-25 17:01:32 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 // Write your solution in this file!
+
 const employee = {
-    name: '',
-    streetAddress: ''
+    name : '',
+    streetAddress : ''
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee};
-    newEmployee[key] = value;
-    return newEmployee;
-};
-
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-};
-
-function  deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = {...employee};
-    delete newEmployee[key];
-    return newEmployee;
+    const newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
 }
 
-function  destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = { ...employee }
+    delete newEmp[key]
+    return newEmp
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
 }
